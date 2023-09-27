658628c4adbc6c1786a22dd8791fcb091681a4b2
bueno <luizhenriquebueno@me.com>
2023-09-26 21:48:22 -0400

    feat: implementing tailwind css

--- src/App.js
+++ src/App.js
@@ -2,87 +2,90 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function removeHighlights(textWithHighlights) {
-  const div = document.createElement("div");
-  div.innerHTML = textWithHighlights;
-  const highlights = div.querySelectorAll(".highlight");
-  highlights.forEach((highlight) => {
-    const textNode = document.createTextNode(highlight.textContent);
-    highlight.parentNode.replaceChild(textNode, highlight);
-  });
-  return div.innerHTML;
+	const div = document.createElement("div");
+	div.innerHTML = textWithHighlights;
+	const highlights = div.querySelectorAll(".highlight");
+	highlights.forEach((highlight) => {
+		const textNode = document.createTextNode(highlight.textContent);
+		highlight.parentNode.replaceChild(textNode, highlight);
+	});
+	return div.innerHTML;
 }
 
 function App() {
-  const [text, setText] = useState({});
+	const [text, setText] = useState({});
 
-  useEffect(() => {
-    fetch("/data/data.json")
-      .then((response) => response.json())
-      .then((data) => {
-        setText(data);
-      })
-      .catch((error) => {
-        console.error("An error occurs when loading content:", error);
-      });
-  }, []);
+	useEffect(() => {
+		fetch("/data/data.json")
+			.then((response) => response.json())
+			.then((data) => {
+				setText(data);
+			})
+			.catch((error) => {
+				console.error("An error occurs when loading content:", error);
+			});
+	}, []);
 
-  useEffect(() => {
-    const addHighlight = (range) => {
-      const span = document.createElement("span");
-      span.className = "highlight";
+	useEffect(() => {
+		const addHighlight = (range) => {
+			const span = document.createElement("span");
+			span.className = "highlight";
 
-      // Check if the range is valid and contains nodes
-      if (range && range.cloneContents().textContent.trim() !== "") {
-        span.textContent = range.toString();
-        range.deleteContents();
-        range.insertNode(span);
-      }
-    };
+			// Check if the range is valid and contains nodes
+			if (range && range.cloneContents().textContent.trim() !== "") {
+				span.textContent = range.toString();
+				range.deleteContents();
+				range.insertNode(span);
+			}
+		};
 
-    const handleMouseUp = () => {
-      const selection = window.getSelection();
-      if (selection.toString().trim() !== "") {
-        addHighlight(selection.getRangeAt(0));
-        selection.empty();
-      }
-    };
+		const handleMouseUp = () => {
+			const selection = window.getSelection();
+			if (selection.toString().trim() !== "") {
+				addHighlight(selection.getRangeAt(0));
+				selection.empty();
+			}
+		};
 
-    const handleClick = (e) => {
-      if (e.target.classList.contains("highlight")) {
-        const span = e.target;
-        const text = document.createTextNode(span.textContent);
-        span.parentNode.replaceChild(text, span);
-      }
-    };
+		const handleClick = (e) => {
+			if (e.target.classList.contains("highlight")) {
+				const span = e.target;
+				const text = document.createTextNode(span.textContent);
+				span.parentNode.replaceChild(text, span);
+			}
+		};
 
-    const columnRight = document.querySelector(".column-right");
+		const columnRight = document.querySelector(".column-right");
 
-    columnRight.addEventListener("mouseup", handleMouseUp);
-    columnRight.addEventListener("click", handleClick);
+		columnRight.addEventListener("mouseup", handleMouseUp);
+		columnRight.addEventListener("click", handleClick);
 
-    return () => {
-      columnRight.removeEventListener("mouseup", handleMouseUp);
-      columnRight.removeEventListener("click", handleClick);
-    };
-  }, []);
+		return () => {
+			columnRight.removeEventListener("mouseup", handleMouseUp);
+			columnRight.removeEventListener("click", handleClick);
+		};
+	}, []);
 
-  return (
-    <div className="App">
-      <h1>{text.textTitle}</h1>
-      <table>
-        <tbody>
-          <tr>
-            <td className="column-left">
-              <p>{removeHighlights(text.textMarked)}</p>
-            </td>
-            <td className="column-right">
-              <p dangerouslySetInnerHTML={{ __html: text.textMarked }}></p>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  );
+	return (
+		<div className="container mx-auto font-sans bg-gray-200 rounded-xl shadow border p-8 m-10">
+			<h1 className="text-center text-3xl text-gray-700 font-bold p-4 mb-5">
+				{text.textTitle}!
+			</h1>
+			<div className="flex flex-col md:flex-row">
+				<div className="column-left p-4">
+					<p className="text-gray-500 text-lg">
+						{removeHighlights(text.textMarked)}
+					</p>
+				</div>
+				<div className="column-right p-4">
+					<p
+						className="text-gray-500 text-lg"
+						dangerouslySetInnerHTML={{ __html: text.textMarked }}
+					></p>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
