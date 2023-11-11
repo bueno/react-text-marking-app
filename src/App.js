79ae838d07d77d8a7e8169c61cb18a11a414ca97
Bueno <luizhenriquebueno@me.com>
2023-11-11 07:19:16 -0500

    fix: fixing test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { LeftColumn, RightColumn } from "./components/ColumnComponent"; 
+import { LeftColumn, RightColumn } from "./components/ColumnComponent";
 import { RemoveHighlights } from "./components/HighlightedText";
 
 function App() {
@@ -18,15 +18,20 @@ function App() {
   }, []);
 
   return (
-    <div className="container mx-auto font-sans bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <h1 className="text-center text-3xl text-gray-700 font-bold p-4 mb-5">
-        {text.textTitle}!
+    <>
+      <h1 className="text-center text-3xl text-gray-700 font-bold p-4">
+        React Text Highlighter App
       </h1>
-      <div className="flex flex-col md:flex-row">
-        <LeftColumn text={RemoveHighlights(text.textMarked)} />
-        <RightColumn text={text.textMarked} />
+      <div className="container mx-auto font-sans bg-gray-200 rounded-xl shadow border p-8 m-2">
+        <h2 className="text-center text-2xl text-gray-500 font-bold p-4 mb-5">
+          {text.textTitle}!
+        </h2>
+        <div className="flex flex-col md:flex-row">
+          <LeftColumn text={RemoveHighlights(text.textMarked)} />
+          <RightColumn text={text.textMarked} />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders React Text Highlighter App header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/React Text Highlighter App/i);
+  expect(headerElement).toBeInTheDocument();
 });
